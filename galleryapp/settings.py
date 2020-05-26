58f5f49c459c32f9f197176f6efd7cd1b7360b50
Edwin Karanu Muiruri <edwin.karanu.muiruri@gmail.com>
2020-05-26 14:55:03 +0300

    Edit Heroku.Set DB name,DEBUG,and SECRET KEY to .env

--- galleryapp/settings.py
+++ galleryapp/settings.py
@@ -12,10 +12,7 @@ https://docs.djangoproject.com/en/3.0/ref/settings/
 
 import os
 import django_heroku
-import dj_database_url
-from decouple import config,Csv
 
-MODE = config("MODE",default = "dev")
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 
@@ -24,34 +21,12 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 # See https://docs.djangoproject.com/en/3.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = config('SECRET_KEY')
+SECRET_KEY = os.environ.get("SECRET_KEY")
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = config('DEBUG', default = "False", cast = bool)
-if config('MODE')=="dev":
-   DATABASES = {
-       'default': {
-           'ENGINE': 'django.db.backends.postgresql_psycopg2',
-           'NAME': config('DB_NAME'),
-           'USER': config('DB_USER'),
-           'PASSWORD': config('DB_PASSWORD'),
-           'HOST': config('DB_HOST'),
-           'PORT': '',
-       }
-       
-   }
-# production
-else:
-   DATABASES = {
-       'default': dj_database_url.config(
-           default=config('DATABASE_URL')
-       )
-   }
+DEBUG = os.environ.get("DEBUG")
 
-db_from_env = dj_database_url.config(conn_max_age=500)
-DATABASES['default'].update(db_from_env)
-
-ALLOWED_HOSTS = config('ALLOWED_HOSTS',cast=Csv)
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
@@ -102,8 +77,19 @@ WSGI_APPLICATION = 'galleryapp.wsgi.application'
 
 # Database
 # https://docs.djangoproject.com/en/3.0/ref/settings/#databases
-
-
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        'NAME': os.environ.get('DB_NAME', 'djangogallery'),
+        'USER': os.environ.get('DB_USER', 'edwin'),
+        'PASSWORD': os.environ.get('DB_PASSWORD', 'EDUcaranow98'),
+        'HOST': os.environ.get('DB_HOST', 'localhost'),
+        'PORT': os.environ.get('DB_PORT', '5432'),
+        'TEST': {
+            'NAME': 'test_djangogallery'
+        }
+    },
+}
 
 
 # Password validation
